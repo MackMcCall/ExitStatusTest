572dd883068cebedbc0a92af96eba09a1adc1b54
Mack McCall <mackmc9924@gmail.com>
2023-07-21 16:08:45 -0600

    Messed with exit status

---
+++ ExitStatusTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ExitStatusTest
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Type \"yay\" or else");
+            string input = Console.ReadLine();
+
+            if (input != "yay")
+            {
+                Console.WriteLine("why didn't you do it?");
+                return 1;
+            }
+            Console.WriteLine("Good Job!");
+            return 0;
+        }
+    }
+}
